c24ed6ef1d9395d71c40418e2cd9a6d8d08979b0
william <w5pan@ucsd.edu>
2022-04-16 03:13:55 -0700

    resize image

--- read.py
+++ read.py
@@ -1,7 +1,16 @@
 import cv2 as cv
 
 img = cv.imread('steam.jpg')
+
+
 def rescaleframe(frame,scale = 0.5):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width,height)
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
 
 cv.imshow('steam',img)
+img_resize = rescaleframe(img,scale=0.1)
+
+cv.imshow('steam_resize',img_resize)
 cv.waitKey(0)
